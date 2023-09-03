dbfecdd0bfd305cbf2407c3146d84e284b401920
Qais Nizami <qais.nizami@gmail.com>
2023-09-03 07:22:59 -0400

    created POST form

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,28 +13,35 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+
+/* app.get("/", (req, res) => {
   res.send("Hello!");
-});
+}); */
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
+/* app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
+}); */
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: req.params.longURL}; /* What goes here? */ 
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {}
